10c9695a79d8d19ed193a8030ae7a37a9207cc26
doojoo9999 <parkbr95@naver.com>
2024-02-13 14:30:10 +0900

    feat : 테스트코드를 위한 h2 db 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,6 +78,9 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+
+    //h2
+    runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/com/teamsparta/dailywrite/domain/message/controller/PostControllerTest.kt
@@ -0,0 +1,4 @@
+package com.teamsparta.dailywrite.domain.message.controller
+
+class PostControllerTest {
+}
